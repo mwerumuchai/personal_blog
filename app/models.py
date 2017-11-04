9e79e3983c789d837a316a0d3bea964b33471557
mwerumuchai <mwerumuchai@gmail.com>
2017-11-04 12:52:48 +0300

    add user class in the models

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# collection of all blogs
 class Blog(db.Model):
     '''
     Blog class define Blog
@@ -36,3 +37,34 @@ class Blog(db.Model):
         '''
         blog = Blog.query.all()
         return blog
+
+# users
+class User(UserMixin,db.Model):
+    '''
+    User class that will help to create new users
+    '''
+    __tablename__ = 'users'
+
+    # add columns
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+    # securing our passwords
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+
+    def __repr__(self):
+        return f'User {self.username}'
