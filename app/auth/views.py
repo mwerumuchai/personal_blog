1734d289017d2058b36750a546fdcd184294990e
mwerumuchai <mwerumuchai@gmail.com>
2017-11-05 04:10:57 +0300

    import user from models

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,redirect,url_for,request,flash
 from . import auth
+from ..models import User
 from flask_login import login_user,logout_user,login_required
 from .forms import RegistrationForm, LoginForm
 from .. import db
